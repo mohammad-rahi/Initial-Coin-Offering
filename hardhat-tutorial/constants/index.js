2f6949d8c4dccee0d994ca1bb7b50532175f621a
mohammad-rahi <mohammadrahi00318@gmail.com>
2022-11-01 00:27:02 +0600

    update contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x894aDfF2E406a5a5d3BEC04c7baBFB11e9bC5527";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nftcollectiondev.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
